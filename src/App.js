0ad4fa3c997285ccbfe80de439c54dd886ed5518
Your Name <jonathandreo@thelewiscollege.edu.ph>
2023-11-27 21:28:59 +0800

    NAPALITAN

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ProdManagement from './ProductManagement';
+import StockManagement from './StockManagement';
+import CategoryManagement from './CategoryManagement';
+import { ProdListProvider } from './ProdListContext';
+import TransactionManagement from './TransactionManagement';
+import { styles } from './Styles';
+
+const App = () => {
+  const [selectedTab, setSelectedTab] = React.useState('Product Management');
+
+  const TabContent = () => {
+    if (selectedTab === 'Product Management') {
+      return <ProdManagement />;
+    } else if (selectedTab === 'Stock Management') {
+      return <StockManagement />;
+    } else if (selectedTab === 'Category Management') {
+      return <CategoryManagement />;
+    } else if (selectedTab === 'Transaction Management') {
+      return <TransactionManagement />;
+    }
+    return null;
+  };
+
+  return (
+    <ProdListProvider>
+      <div>
+        <ul style={{ display: 'flex',listStyle: 'none', justifyContent: 'center'}}>
+          <li>
+            <button class="bg-blue-400 p-2"onClick={() => setSelectedTab('Product Management')} style={styles.sty}>Product Management</button>
+          </li>
+          <li style={{ marginRight: '10px' }}>
+            <button class="bg-blue-400 p-2" onClick={() => setSelectedTab('Stock Management')} style={styles.sty}>Stock Management</button>
+          </li>
+          <li style={{ marginRight: '10px' }}>
+            <button class="bg-blue-400 p-2"onClick={() => setSelectedTab('Category Management')} style={styles.sty}>Category Management</button>
+          </li>
+          <li style={{ marginRight: '10px' }}>
+            <button class="bg-blue-400 p-2"onClick={() => setSelectedTab('Transaction Management')}style={styles.sty}>Transaction Management</button>
+          </li>
+        </ul>
+        <hr />
+        {TabContent()}
+      </div>
+    </ProdListProvider>
+  );
+};
+
+export default App;
---
+++ src/Styles.js
@@ -0,0 +1,63 @@
+const inputStyles = {
+    width: '100%',
+    padding: '10px',
+    margin: '5px 0 22px 0',
+    display: 'inline-block',
+    border: '1px solid #c2c0ca',
+    borderRadius: '8px',
+    background: '#f7f7f7',
+    fontFamily: 'Montserrat, Arial, Helvetica, sans-serif',
+  };
+
+
+
+const buttonStyles = {
+    backgroundColor: '#0eb7f4',
+    color: 'white',
+    padding: '14px 20px',
+    margin: '8px 0',
+    border: 'none',
+    cursor: 'pointer',
+    width: '100%',
+    opacity: '0.9',
+    fontSize: '16px',
+    fontFamily: 'Montserrat, Arial, Helvetica, sans-serif',
+    borderRadius: '10px',
+   
+  };
+
+  const sty = {
+
+    backgroundColor: '#0eb7f4',
+    color: 'black',
+    display: 'block',
+    padding: '14px 20px',
+    margin: '5px 0',
+    border: 'none',
+    cursor: 'pointer',
+    width: '320px',
+    opacity: '80%',
+    fontSize: '20px',
+    fontFamily: 'Montserrat, Arial, Helvetica, sans-serif',
+    borderRadius: '10px',
+    marginRight:'10px',
+    
+  };
+  const hover={
+    backgroundColor:'#ddd',
+    border:'none'
+
+  }
+
+
+
+  
+
+export const styles = {
+    inputStyles : inputStyles,
+    buttonStyles : buttonStyles,
+    sty: sty,
+    hover:hover
+   
+    
+ };
